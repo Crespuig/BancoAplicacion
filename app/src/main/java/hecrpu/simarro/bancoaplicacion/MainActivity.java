3cc61629a4500c24534067a9501181353ad598e7
Héctor <hectorcrespuig@gmail.com>
2020-10-17 17:20:43 +0200

    Actualizado con animaciones

--- app/src/main/java/hecrpu/simarro/bancoaplicacion/MainActivity.java
+++ app/src/main/java/hecrpu/simarro/bancoaplicacion/MainActivity.java
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,13 +18,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        //Agregar animaciones
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView deTextView = findViewById(R.id.vankioTextView);
+        ImageView logoImageView = findViewById(R.id.logoImageView);
+
+        deTextView.setAnimation(animacion2);
+        logoImageView.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
     }
 
-    public void onClickDashboard(View view) {
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivityForResult(intent, 0);
 
-    }
 }
--- app/src/main/java/hecrpu/simarro/bancoaplicacion/PrincipalActivity.java
+++ app/src/main/java/hecrpu/simarro/bancoaplicacion/PrincipalActivity.java
@@ -2,7 +2,9 @@ package hecrpu.simarro.bancoaplicacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+    }
+
+    public void onClickBtnSalir(View view) {
+        Intent intent = new Intent(PrincipalActivity.this, MainActivity.class);
+        startActivityForResult(intent, 0);
+
+    }
+
+    public void onClickCambiarClave(View view) {
+        Intent intent = new Intent(PrincipalActivity.this, CambiarClave.class);
+        startActivityForResult(intent, 0);
+
     }
 }
