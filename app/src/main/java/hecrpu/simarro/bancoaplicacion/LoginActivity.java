62f03b66193d427945b8fd139fbe274299a27c04
crespuig <hectorcrespuig@gmail.com>
2020-10-28 10:47:35 +0100

    Inicio validación LoginActivity

--- app/src/main/java/hecrpu/simarro/bancoaplicacion/LoginActivity.java
+++ app/src/main/java/hecrpu/simarro/bancoaplicacion/LoginActivity.java
@@ -5,18 +5,63 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import hecrpu.simarro.bancoaplicacion.bd.MiBD;
+import hecrpu.simarro.bancoaplicacion.bd.MiBancoOperacional;
+import hecrpu.simarro.bancoaplicacion.pojo.Cliente;
+
+public class LoginActivity extends AppCompatActivity /*implements View.OnClickListener*/ {
+    EditText usuario;
+    EditText password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 
     public void onClickDashboard(View view) {
+        usuario = (EditText) findViewById(R.id.usuario);
+        password = (EditText) findViewById(R.id.password);
+
+        MiBancoOperacional api = MiBancoOperacional.getInstance(this.getApplicationContext());
+        Cliente c = new Cliente();
+
+        if (usuario.getText() == null || password.getText() == null) {
+            Toast.makeText(LoginActivity.this, "Los campos njo pueden estar vacios", Toast.LENGTH_SHORT).show();
+        } else {
+            api.login(c);
+        }
+
         Intent intent = new Intent(LoginActivity.this, PrincipalActivity.class);
         startActivityForResult(intent, 0);
 
+
     }
 }
+
+
+    /*@Override
+    public void onClick(View view) {
+        usuario = (EditText) findViewById(R.id.usuario);
+        password = (EditText) findViewById(R.id.password);
+
+        MiBancoOperacional api = MiBancoOperacional.getInstance(this.getApplicationContext());
+        Cliente c  = new Cliente();
+
+        if (usuario.getText() == null || password.getText() ==null){
+            Toast.makeText(LoginActivity.this, "Los campos njo pueden estar vacios", Toast.LENGTH_SHORT).show();
+        }else {
+            api.login(c);
+        }
+
+        Intent intent = new Intent(LoginActivity.this, PrincipalActivity.class);
+        startActivityForResult(intent, 0);
+
+    }*/
